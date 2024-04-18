db0c8692a444cdec7acf0e78da9309778ed657f7
eivindholvik <eivind.holvik@noroff.no>
2024-04-18 11:23:27 +0200

    complete assignment

--- array.js
+++ array.js
@@ -1,3 +1,18 @@
-export function largestNumber(arr) {}
+export function largestNumber(arr) {
+  let biggest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > biggest) biggest = arr[i];
+  }
+  return biggest;
+}
 
-export function largestSum(arr, n) {}
+export function largestSum(arr, n) {
+  if (arr.length < n) return;
+  arr.sort((a, b) => b - a);
+  let out = 0;
+  for (let i = 0; i < n; i++) {
+    console.log(arr[i]);
+    out += arr[i];
+  }
+  return out;
+}
